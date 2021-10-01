7c8c2b7a0e49aa14b5dd6cf00f96c00f52169739
Anu-G <fittryanto7@gmail.com>
2021-10-01 20:37:53 +0700

    check app.listen

--- main.go
+++ main.go
@@ -1,27 +1,28 @@
 package main
 
 import (
-	"log"
-	"os"
+	//"log"
+	//"os"
 
 	"server_deploy/route"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
-	"github.com/joho/godotenv"
+	//"github.com/joho/godotenv"
 )
 
 func main() {
 	app := SetupFiber()
-	app.Listen(os.Getenv("APP_PORT"))
+	//app.Listen(os.Getenv("APP_PORT"))
+	app.Listen(":3000")
 }
 
-func init() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("error loading .env file, ", err)
-	}
-}
+// func init() {
+// 	err := godotenv.Load(".env")
+// 	if err != nil {
+// 		log.Fatal("error loading .env file, ", err)
+// 	}
+// }
 
 func SetupFiber() *fiber.App {
 	app := fiber.New(fiber.Config{})
